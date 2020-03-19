c1f2a0162e4e0f2e6b48dd715571cee9071fd049
cheldasan <chelda3230@gmail.com>
2020-03-19 21:09:42 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 function App() {
-  const profiles = [
-    {
-      name: "Taro",
-      age: 10
-    },
-    {
-      name: "Hanako",
-      age: 5
-    },
-    {
-      name: "NoName",
-      age: 3
-    }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
+  return <Counter></Counter>
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old.</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  hundlePlusButton = () => {
+    // this.state = { count: this.state.count + 1 }
+    // setStateはrenderが実行されるので、必ず状態を変更する時には、この関数を使う必要がある。
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  hundleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
