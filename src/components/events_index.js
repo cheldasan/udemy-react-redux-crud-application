bfae5afa6122436a357bac905fb58054cf2eb219
cheldasan <chelda3230@gmail.com>
2020-03-25 21:32:20 +0900

    implement events

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash'
 
-import { increment, decrement } from '../actions';
+import { readEvents } from '../actions';
 
 class EventsIndex extends Component {
   componentDidMount() {
-    console.log("hi!")
     this.props.readEvents()
   }
 
-  render() {
-    const props = this.props
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
 
+  render() {
     return (
-      <React.Fragment>
-        <div>value: { props.value }</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  value: state.count.value
-})
+const mapStateToProps = state => ({ events: state.events })
 
 const mapDispatchToProps = ({ readEvents })
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 //application内のreducerを総括する
 //reducerは、actionを受けてstateを変更するの為のメソッドです
 import { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
 // export default combineReducers({ foo, bar, baz })のようにも書ける
