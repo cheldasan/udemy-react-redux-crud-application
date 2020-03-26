b6a79cf58cf298a663c3092dc448a383acf3d79b
cheldasan <chelda3230@gmail.com>
2020-03-26 21:14:07 +0900

    implement events-new

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 //application内のreducerを総括する
 //reducerは、actionを受けてstateを変更するの為のメソッドです
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 // export default combineReducers({ foo, bar, baz })のようにも書ける
